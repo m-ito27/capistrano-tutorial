9f00d437bafdd11c5b3a70f3882220679e738aa2
Ito Masaki <msk.nfl.teabow12@gmail.com>
2023-01-30 20:54:02 +0900

    deploy.rb変更

--- config/deploy.rb
+++ config/deploy.rb
@@ -32,7 +32,7 @@ set :unicorn_config_path, -> { "#{current_path}/config/unicorn.rb" }
 # ブランチ指定
 set :branch, 'main'
 
-append :linked_files, 'config/credentials/production.key'
+set :linked_files, 'config/credentials/production.key'
 
 # Unicornを再起動するための記述
 after 'deploy:publishing', 'deploy:restart'
